28db9468d85e78eadb69a6b7b010e3c05b108ecc
Hakob <hakob-hakobyan-1991@mail.ru>
2022-12-17 18:36:35 +0300

    HomeWork1

---
+++ src/homeWork/homework1/FigurePainter.java
@@ -0,0 +1,32 @@
+package homeWork.homework1;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+
+        System.out.println("Figure 1");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Figure 2");
+        for (int i = 5; i > 0; i--) {
+            for (int j = 1; j <= 5; j++) {
+                if (i <= j) {
+                    System.out.print(" #");
+                } else System.out.print("  ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Figure 3");
+        for (int i = 5; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("# ");
+            }
+            System.out.println();
+        }
+    }
+}
