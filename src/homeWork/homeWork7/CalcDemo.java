bac550a66775284fb66969887c01c9e3ec7278fb
Hakob <hakob-hakobyan-1991@mail.ru>
2023-01-14 13:27:55 +0300

    Home work6 Class Calculator

---
+++ src/homeWork/homeWork7/CalcDemo.java
@@ -0,0 +1,15 @@
+package homeWork.homeWork7;
+
+public class CalcDemo {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.divide(4,2));
+        System.out.println(calc.divide(4,0));
+        System.out.println(calc.minus(2,4));
+        System.out.println(calc.multiply(4,2));
+        System.out.println(calc.plus(3,5));
+
+    }
+
+}
---
+++ src/homeWork/homeWork7/Calculator.java
@@ -0,0 +1,25 @@
+package homeWork.homeWork7;
+
+public class Calculator {
+
+    double plus(int a, int b) {
+        return a + b;
+    }
+
+    double minus(int a, int b) {
+        return a - b;
+    }
+
+    double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("cannot be divided by zero");
+            return -1;
+        }
+        return a / b;
+    }
+
+    double multiply(int a, int b) {
+        return a * b;
+    }
+
+}
