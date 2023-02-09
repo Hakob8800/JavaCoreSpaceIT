67b292227204978bf7bbb40059d8fed1b63fea06
Hakob <hakob-hakobyan-1991@mail.ru>
2023-02-09 22:20:11 +0300

    home work Employee(some changes)

--- src/homeWork/employee/EmployeeDemo.java
+++ src/homeWork/employee/EmployeeDemo.java
@@ -9,57 +9,35 @@ public class EmployeeDemo {
         EmployeeStorage employeeStorage = new EmployeeStorage();
         boolean isRun = true;
 
-        while (isRun){
-            System.out.println("----------------------------------------");
-            System.out.println("For exit press 0");
-            System.out.println("For add employee press 1");
-            System.out.println("For print all employees press 2");
-            System.out.println("For search employee by ID press 3");
-            System.out.println("For search employee by company press 4");
-            System.out.println("For search employee by salary press 5");
-            System.out.println("----------------------------------------");
-            String command = scanner.nextLine();
-
+        while (isRun) {
+            String command = employeeStorage.chooseCommand();
             switch (command) {
                 case "0":
                     isRun = false;
                     break;
                 case "1":
-                    System.out.println("Please enter name");
-                    String name = scanner.nextLine();
-                    System.out.println("Please enter surName");
-                    String surName = scanner.nextLine();
-                    System.out.println("Please enter ID");
-                    String ID = scanner.nextLine();
-                    System.out.println("Please enter salary");
-                    double salary = Double.parseDouble(scanner.nextLine());
-                    System.out.println("please enter position");
-                    String position = scanner.nextLine();
-                    System.out.println("Please enter company");
-                    String company = scanner.nextLine();
-                    Employee employee = new Employee(name,surName,ID,salary,position,company);
+                    Employee employee = employeeStorage.createEmployee();
                     employeeStorage.add(employee);
-                    System.out.println("Employee successfully added");
                     break;
-                case"2":
+                case "2":
                     employeeStorage.print();
                     break;
-                case"3":
-                    System.out.println("Please enter key word for ID");
-                    String keyWordID = scanner.nextLine();
-                    employeeStorage.searchByEmployeeID(keyWordID);
+                case "3":
+                    System.out.println(employeeStorage.searchByEmployeeID());
                     break;
-                case"4":
-                    System.out.println("Please enter key word for company");
-                    String keyWordCompany = scanner.nextLine();
-                    employeeStorage.searchByCompany(keyWordCompany);
+                case "4":
+                    for (Employee employee1 : employeeStorage.searchByCompany()) {
+                        if (employee1 != null) {
+                            System.out.println(employee1);
+                        }
+                    }
                     break;
-                case"5":
-                    System.out.println("Please enter MIN salary for search");
-                    double min = Double.parseDouble(scanner.nextLine());
-                    System.out.println("Please enter MAX salary for search");
-                    double max = Double.parseDouble(scanner.nextLine());
-                    employeeStorage.searchBySalary(min,max);
+                case "5":
+                    for (Employee employee1 : employeeStorage.searchBySalary()) {
+                        if (employee1 != null) {
+                            System.out.println(employee1);
+                        }
+                    }
                     break;
                 default:
                     System.out.println("Wrong command! Please try again.");
--- src/homeWork/employee/EmployeeStorage.java
+++ src/homeWork/employee/EmployeeStorage.java
@@ -1,7 +1,10 @@
 package homeWork.employee;
 
+import java.util.Scanner;
+
 public class EmployeeStorage {
 
+    Scanner scanner = new Scanner(System.in);
     private Employee[] array = new Employee[10];
     private int size;
 
@@ -9,7 +12,10 @@ public class EmployeeStorage {
         if (size == array.length) {
             extend();
         }
-        array[size++] = employee;
+        if (employee != null) {
+            array[size++] = employee;
+            System.out.println("Employee successfully added");
+        }
     }
 
 
@@ -27,48 +33,95 @@ public class EmployeeStorage {
         array = tmp;
     }
 
-    public void searchByEmployeeID(String keyWordID) {
-        boolean found = false;
-        for (int i = 0; i < size; i++) {
-
-            Employee employee = array[i];
-            if (employee.getEmployeeID().toLowerCase().contains(keyWordID.toLowerCase())) {
-                found = true;
-                System.out.println(employee);
+    public Employee searchByEmployeeID() {
+        System.out.println("Please enter key word for ID");
+        String ID = scanner.nextLine();
+        for (Employee employee : array) {
+            if (employee != null) {
+                if (employee.getEmployeeID().equals(ID)) {
+                    return employee;
+                }
             }
         }
-        if (!found) {
-            System.out.println("By keyword \"" + keyWordID + "\" nothing founds");
-        }
+        System.out.println("By ID \'" + ID + "\' nothing founds");
+        return null;
     }
 
-    public void searchByCompany(String keyWordCompany) {
+    public Employee[] searchByCompany() {
         boolean found = false;
+        Employee[] arrayForReturn = new Employee[size];
+        int j = 0;
+        System.out.println("Please enter key word for company");
+        String keyWordCompany = scanner.nextLine();
         for (int i = 0; i < size; i++) {
-
             Employee employee = array[i];
             if (employee.getCompany().toLowerCase().contains(keyWordCompany.toLowerCase())) {
                 found = true;
-                System.out.println(employee);
+                arrayForReturn[j++] = employee;
             }
         }
         if (!found) {
             System.out.println("By keyword \"" + keyWordCompany + "\" nothing founds");
         }
+        return arrayForReturn;
     }
 
-    public void searchBySalary(double min, double max) {
+    public Employee[] searchBySalary() {
+        Employee[] arrayForSearch = new Employee[size];
+        int j = 0;
         boolean found = false;
+        System.out.println("Please enter MIN salary for search");
+        double min = Double.parseDouble(scanner.nextLine());
+        System.out.println("Please enter MAX salary for search");
+        double max = Double.parseDouble(scanner.nextLine());
         for (int i = 0; i < size; i++) {
 
             Employee employee = array[i];
             if (employee.getSalary() >= min && employee.getSalary() <= max) {
                 found = true;
-                System.out.println(employee);
+                arrayForSearch[j++] = employee;
             }
         }
         if (!found) {
-            System.out.println("There is not any employee whit salary in range from " + min + " to " + max);
+            System.out.println("There is not any employee with salary in range from " + min + " to " + max);
         }
+        return arrayForSearch;
+    }
+
+    public String chooseCommand() {
+        System.out.println("----------------------------------------");
+        System.out.println("For exit press 0");
+        System.out.println("For add employee press 1");
+        System.out.println("For print all employees press 2");
+        System.out.println("For search employee by ID press 3");
+        System.out.println("For search employee by company press 4");
+        System.out.println("For search employee by salary press 5");
+        System.out.println("----------------------------------------");
+        return scanner.nextLine();
+    }
+
+    public Employee createEmployee() {
+        System.out.println("Please enter name");
+        String name = scanner.nextLine();
+        System.out.println("Please enter surName");
+        String surname = scanner.nextLine();
+        System.out.println("Please enter ID");
+        String ID = scanner.nextLine();
+        for (Employee employee : array) {
+            if (employee != null) {
+                if (employee.getEmployeeID().equals(ID)) {
+                    System.out.println("ID " + ID + " has already used");
+                    return null;
+                }
+            }
+        }
+        System.out.println("Please enter salary");
+        double salary = Double.parseDouble(scanner.nextLine());
+        System.out.println("please enter position");
+        String position = scanner.nextLine();
+        System.out.println("Please enter company");
+        String company = scanner.nextLine();
+
+        return new Employee(name, surname, ID, salary, position, company);
     }
 }
