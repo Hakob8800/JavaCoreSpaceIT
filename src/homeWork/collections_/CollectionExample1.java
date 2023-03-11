f883fe19fef24b68da54e33bc34408302b0597de
Hakob <hakob-hakobyan-1991@mail.ru>
2023-03-11 10:09:12 +0300

    Homework Collections

--- src/homeWork/collections_/CollectionExample1.java
+++ src/homeWork/collections_/CollectionExample1.java
@@ -17,9 +17,9 @@ public class CollectionExample1 {
 
         checkIsin(names);
         deleteName(names);
-        try{
+        try {
             addByIndex(names);
-        }catch (ArrayIndexOutOfBoundsException e){
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("You choose wrong index!");
         }
         System.out.println(names);
@@ -36,26 +36,25 @@ public class CollectionExample1 {
         }
     }
 
-    private static void deleteName(List<String> names){
+    private static void deleteName(List<String> names) {
         System.out.println("Please input name for delete from list");
         String name = scanner.nextLine();
-        if(!names.contains(name)){
-            System.out.println("there is not "+name);
-        }
-        else {
+        if (!names.contains(name)) {
+            System.out.println("there is not " + name);
+        } else {
             names.remove(name);
             System.out.println(names);
         }
     }
 
-    private static void addByIndex(List<String> names) throws ArrayIndexOutOfBoundsException{
-        System.out.println("Please input index from range 0 to "+names.size());
+    private static void addByIndex(List<String> names) throws ArrayIndexOutOfBoundsException {
+        System.out.println("Please input index from range 0 to " + names.size());
         int index = Integer.parseInt(scanner.nextLine());
-        if(index>names.size()){
+        if (index > names.size() || index < 0) {
             throw new ArrayIndexOutOfBoundsException();
         }
         System.out.println("Please input name");
         String name = scanner.nextLine();
-        names.add(index,name);
+        names.add(index, name);
     }
 }
