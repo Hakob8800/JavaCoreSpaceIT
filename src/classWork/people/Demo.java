cf6a06966e45c16fd680a1a140a248e1f699cbe4
Hakob <hakob-hakobyan-1991@mail.ru>
2023-03-13 21:41:33 +0300

    some changes

--- src/classWork/people/Demo.java
+++ src/classWork/people/Demo.java
@@ -1,5 +1,7 @@
 package classWork.people;
 
+import java.util.Collections;
+
 public class Demo {
 
     public static void main(String[] args) {
@@ -18,7 +20,6 @@ public class Demo {
             if(arr[i]!=null){
                 System.out.println(arr[i].toString());
             }
-            else continue;
         }
 
     }
